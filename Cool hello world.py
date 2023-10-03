637a8f206c799dbc0dd1cb8b11726753730c943e
iCrazyOG <87263238+iCrazyOG@users.noreply.github.com>
2023-10-03 00:55:37 -0400

    Update Cool hello world.py

--- Cool hello world.py
+++ Cool hello world.py
@@ -1,31 +1,48 @@
+'''
+I saw this in a reel, and decided to try and make it myself
+'''
+
 import time, random, string
-target=input("Enter a word\n")
-#target="Hello World"
+
 alphabet=[' ']
 for a in string.printable:
     alphabet.append(a)
-final=""
-choice=input("do you want the basic version or cool version?\n")
-if choice.lower().find("cool")>-1:
-    while (final!=target):
-        for b in range(0,len(target)):
-            c=random.choice(alphabet)
-            print(final+c)
-            time.sleep(0.01)
-            if (final+c)==target[0:len(final+c)]:
-                final=final+c
-            if target==final:
-                break
-else:
-    while (final!=target):
-        for b in alphabet:
-            print(final+b)
-            time.sleep(0.01)
-            if (final+b)==target[0:len(final+b)]:
-                final=final+b
-            if target==final:
-                break
-
-content=input("\n")
+    
+def word_gen():
+    target=input("Enter a word\n")
+    final=""
+    while(True):
+        choice=input("Do you want the basic version or cool version?\n")
+        if (choice.lower().find("cool")==-1) and (choice.lower().find("basic")==-1):
+            print("try again")
+        else:
+            break
+        
+    if choice.lower().find("cool")>-1:
+        while (final!=target):
+            for b in range(0,len(target)):
+                c=random.choice(alphabet)
+                print(final+c)
+                time.sleep(0.01)
+                if (final+c)==target[0:len(final+c)]:
+                    final=final+c
+                if target==final:
+                    break
+    else:
+        while (final!=target):
+            for b in alphabet:
+                print(final+b)
+                time.sleep(0.01)
+                if (final+b)==target[0:len(final+b)]:
+                    final=final+b
+                if target==final:
+                    break
+                
+while (True):
+    word_gen()
+    again=input("Yes or No: Do you want to go again?\n")
+    if ("no" in again.lower()):
+        print("Thank you!")
+        break;
         
     
